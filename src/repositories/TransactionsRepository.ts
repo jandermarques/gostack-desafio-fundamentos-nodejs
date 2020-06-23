7900ff8667719539774258cb6f05cd7d4cf81a87
Jander Marques <jander.marques@gmail.com>
2020-06-23 11:01:48 -0300

    first commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -9,20 +15,49 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(function (accum, curr) {
+      if (curr.type === 'income') {
+        return accum + curr.value;
+      }
+      return accum;
+    }, 0);
+
+    const outcome = this.transactions.reduce(function (accum, curr) {
+      if (curr.type === 'outcome') {
+        return accum + curr.value;
+      }
+      return accum;
+    }, 0);
+
+    this.balance = { income, outcome, total: income - outcome };
+
+    const { balance } = this;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
